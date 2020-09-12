e2bcf075540dc4f2f9260746c54f6ba0c3a57304
Ian <ianwfuzzy@gmail.com>
2020-09-11 22:37:32 -0400

    Upadting master

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,6 @@
 const today = moment().format('dddd MMMM DD')
       console.log(today) //"September 11th 2020"
 $('.currentDate').append(today)
+function schedule () {
+    var input = document.getElementById("saveBtn1")
+}
