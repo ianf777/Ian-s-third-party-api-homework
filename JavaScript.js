fd044129a38787880089973455d35ecccdf3bfe8
Ian <ianwfuzzy@gmail.com>
2020-09-11 23:23:23 -0400

    updating master

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,11 @@
 const today = moment().format('dddd MMMM DD')
       console.log(today) //"September 11th 2020"
 $('.currentDate').append(today)
-function schedule () {
-    var input = document.getElementById("saveBtn1")
-}
+$("#saveBtn1").on("click", function() {
+    // get nearby values
+    localStorage.setItem('input1', 'input1')
+    var value = (saveBtn1);
+    var time = (saveBtn1);
+    // save in localStorage
+    localStorage.setItem(time, value);
+  });
